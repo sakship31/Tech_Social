89ec26f246e666e6140b5ef37edc65c31e9d0bf1
sakship31 <pandey.sk@somaiya.edu>
2020-07-16 00:57:14 +0530

    minor bug fixes

--- mysite/posts/urls.py
+++ mysite/posts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r"comment/in/(?P<pk>\d+)/$", views.CreateComment.as_view(), name="create2"),
     url(r"by/(?P<username>[-\w]+)/$",views.UserPosts.as_view(),name="for_user"),
     url(r"by/(?P<username>[-\w]+)/(?P<pk>\d+)/$",views.PostDetail.as_view(),name="single"),
+    url(r"comment/delete/(?P<pk>\d+)/$",views.DeleteComment.as_view(),name="delete_comment"),
     url(r"delete/(?P<pk>\d+)/$",views.DeletePost.as_view(),name="delete"),
-    url(r"comment/delete/(?P<pk>\d+)/$",views.DeleteComment.as_view(),name="delete1"),
 ]
